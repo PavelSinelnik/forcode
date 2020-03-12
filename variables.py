72efc50afdab12b7dd9ef95c00bd42d62f5388d6
pavel11111111111111111111 <55051040+pavel11111111111111111111@users.noreply.github.com>
2020-03-12 17:31:24 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+
+
+
+from git import Repo
+
+Repo.clone_from('https://github.com/pavel11111111111111111111/test-web-service', 'C:\\Users\\User\\Desktop\\cloudForm')
+
+#jungle_zip = zipfile.ZipFile('NEWsave_device_infoNEW.zip', 'w')
+#jungle_zip.write('save_device_info.py', compress_type=zipfile.ZIP_DEFLATED)
+
+#jungle_zip.close()
